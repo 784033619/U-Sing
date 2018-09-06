9ccb40304607e9d60dfc54001a955b8d8ebc2cc6
QQQ <2540343312@qq.com>
2018-09-06 17:46:41 +0800

    工具类

---
+++ src/main/java/com/QST/Using/Util/StateAndMessage/StateAndMessage.java
@@ -0,0 +1,13 @@
+package com.QST.Using.Util.StateAndMessage;
+
+public class StateAndMessage {
+    public static final Integer FAIL = 0;
+    public static final Integer SUCCESS = 1;
+
+    public static final String REGISTFMESSAGE = "注册失败";
+    public static final String REGISTSMESSAGE = "注册成功";
+
+
+    public static final String LOGINFMESSAGE = "登录失败";
+    public static final String LOGINSMESSAGE = "登录成功";
+}
