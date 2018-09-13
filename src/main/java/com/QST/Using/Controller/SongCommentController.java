9fa7f47ac64f50cc546b07a2a19b6e3a522d359f
LX <784033619@qq.com>
2018-09-13 10:46:22 +0800

    modify getCommentsByPraiseTime function,add getCommentsByCreatTime function,doesn't get user information next-->put user(userName,pic) into these function

--- src/main/java/com/QST/Using/Controller/SongCommentController.java
+++ src/main/java/com/QST/Using/Controller/SongCommentController.java
@@ -19,10 +19,17 @@ package com.QST.Using.Controller;
 public class SongCommentController {
     @Resource(name = "songCommentService")
     private SongCommentService commentService;
-    @RequestMapping(value = "getComments")
-    public Result getSingRankList(){
-        List<SongComment> songComments = commentService.getSongComments();
-        System.out.println(songComments.size());
+    @RequestMapping(value = "getCommentsByPraiseTime")
+    public Result getCommentsByPraiseTime(){
+        List<SongComment> songComments = commentService.getSongCommentsByPraiseTime();
+        if(songComments.isEmpty()){
+            return new Result(StateAndMessage.FAIL,null,null);
+        }
+        return new Result(StateAndMessage.SUCCESS,null,songComments);
+    }
+    @RequestMapping(value = "getCommentsByCreateTime")
+    public Result getCommentsByCreateTime(){
+        List<SongComment> songComments = commentService.getSongCommentsByCreateTime();
         if(songComments.isEmpty()){
             return new Result(StateAndMessage.FAIL,null,null);
         }
